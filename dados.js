1c9ff9e66c2fe2e39b5ff65681ad1ef75c61e6d6
Duvieiraa <arnaldojosev99@gmail.com>
2024-09-07 22:20:30 -0300

    Subindo projeto

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        nome: "Renato Cariani",
+        titulo: "O maior nome do fisiculturismo brasileiro",
+        descricao: "Um dos principais nomes do mundo fitness...",
+        link: "https://pt.wikipedia.org/wiki/Renato_Cariani",
+        tags: "cariani fisiculturismo musculação treino",
+        dataNascimento: "1980-01-01",
+        modalidade: "fisiculturismo"
+    },
+    {
+        nome: "Beatriz Souza",
+        titulo: "Campeã Olímpica de Judô",
+        descricao: "Judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024...",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô olimpíadas +78kg",
+        dataNascimento: "1995-04-12",
+        modalidade: "judô"
+    },
+];
